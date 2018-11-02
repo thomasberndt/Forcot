3aa21737ceb06fec062fab7bba348238bf280bd3
Thomas Berndt <thomas.andreas.berndt@gmail.com>
2018-11-02 16:31:36 +0800

    save all

--- matlab/LoadAndProcessPrincetonForc.m
+++ matlab/LoadAndProcessPrincetonForc.m
@@ -3,6 +3,6 @@ function princeton = LoadAndProcessPrincetonForc(filename)
     princeton.correctedM = DriftCorrection(princeton);
     princeton.grid = RegularizeForcGrid(princeton); 
     princeton.forc = SmoothForcFft(princeton);
-    princeton.forc.maxHc = 0.9*princeton.forc.maxHc;
-    princeton.forc.maxHu = 0.9*princeton.forc.maxHu;
+    princeton.forc.maxHc = round(0.9*princeton.forc.maxHc,4);
+    princeton.forc.maxHu = round(0.9*princeton.forc.maxHu,4);
 end
--- matlab/OpenForcDialog.m
+++ matlab/OpenForcDialog.m
@@ -1,5 +1,10 @@
-function [files, pathname, ext, n] = OpenForcDialog()
-    [filename, pathname] = uigetfile('*.frc');
+function [files, pathname, ext, n] = OpenForcDialog(pathname)
+    if nargin == 1 && ~isempty(pathname)
+        mask = fullfile(pathname, '*.frc');
+    else 
+        mask = '*.frc'; 
+    end
+    [filename, pathname] = uigetfile(mask);
     [~, ~, ext] = fileparts(filename); 
     files = dir(sprintf('%s/*%s', pathname, ext)); 
     files = {files.name};
