be17c9e2deaa84809ea2deaa478000ac477fdd0f
Thomas Berndt <thomas.andreas.berndt@gmail.com>
2019-03-22 13:28:08 +0800

    installing latet Mac version supported

--- matlab/GetCurrentVersion.m
+++ matlab/GetCurrentVersion.m
@@ -1,6 +1,11 @@
 function version = GetCurrentVersion()
 
-    thisversion = 'Forcot_WebInstaller_v0.1.4-alpha.exe';
+    thisversion = '';
+    if ismac 
+        thisversion = 'Forcot_MacInstaller_v0.1.5-alpha.exe';
+    elseif ispc
+        thisversion = 'Forcot_WebInstaller_v0.1.5-alpha.exe';
+    end
 
     try
         stufftoload = load(fullfile(tempdir, 'Forcot_version.mat')); 
